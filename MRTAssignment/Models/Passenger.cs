3576b3050220e953216042458a373674e8b78d1b
othmansafwan <57385431+othmansafwan@users.noreply.github.com>
2020-05-05 09:08:29 +0800

    Initial commit

---
+++ MRTAssignment/Models/Passenger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MRTAssignment.Models
+{
+    public class Passenger
+    {
+    }
+
+    public class PassengerModel
+    {
+        public String idCard { get; set; }
+        public String passportNo { get; set; }
+        public String name { get; set; }
+        public String email { get; set; }
+
+        public double ticketPrice { get; set; }
+        public int ticketQty { get; set; }
+        public String ticketType { get; set; }
+        public String passengerType { get; set; }
+
+        public String stationFrom { get; set; }
+        public String stationTo { get; set; }
+    }
+}
